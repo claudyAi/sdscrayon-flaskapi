7d2e3d5f52c5846ba716881d7b7c49d234fea08b
Claudiapotato <93199737+Claudiapotato@users.noreply.github.com>
2023-11-14 21:36:46 +0800

    backend works on 127.0.0.1:8000, frontend works on localhost:3000

--- api/index.py
+++ api/index.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/")
+def hello():
+    return "FUCKING WORKS"
+
+
+if __name__ == "__main__":
+    app.run()
