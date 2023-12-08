8cf252b29c8110d0ab541be621607a8b89cd3ee5
MandisLoh <mandisloh13@gmail.com>
2023-12-09 02:38:14 +0800

    update appendNewToName.ts

--- utils/appendNewToName.ts
+++ utils/appendNewToName.ts
@@ -2,6 +2,8 @@ export default function appendNewToName(name: string) {
   let insertPos = name.indexOf(".");
   let newName = name
     .substring(0, insertPos)
-    .concat("-new", name.substring(insertPos));
+    .concat("_new", name.substring(insertPos))
+    .replace("/preds/","")
+    .replace(".jpg", ".tif")
   return newName;
 }
