692d8c427cfebb215ac13b911978cda84c6c84d1
seaweedsaltedfries <earthglitch@gmail.com>
2023-12-10 21:21:21 +0800

    reverted href removal, fixed colour issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex flex-col xs:flex-row justify-between items-center w-full mt-3 border-b pb-7 sm:px-4 px-2 border-gray-500 gap-2">
-      <div className="flex space-x-2">
+      <Link style={{textDecoration:"none"}} href="/" className="flex space-x-2">
         <Image
           alt="header text"
           src="/crayon-logo.png"
@@ -12,10 +12,10 @@ export default function Header() {
           width={24}
           height={24}
         />
-        <h1 className="sm:text-3xl text-xl font-bold ml-2 tracking-tight">
+        <h1 style={{color:"white"}} className="sm:text-3xl text-xl font-bold ml-2 tracking-tight">
           Crayon
         </h1>
-      </div>
+      </Link>
       {/* <a
         className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-500 bg-blue-600 font-medium transition"
         href="https://github.com/Nutlope/roomGPT"
